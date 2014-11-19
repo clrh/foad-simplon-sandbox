287d82a720421ecd83c3da029c5b578bd0c8f342
Claire Hernandez <claire.hernandez@biblibre.com>
2014-11-19 17:09:19 +0100

    red

--- number_to_roman/python/numberToRoman.py
+++ number_to_roman/python/numberToRoman.py
@@ -6,6 +6,8 @@ import unittest
 class TestLags(unittest.TestCase):
     def test_test(self):
         self.assertEqual(True, True)
+    def test_1_donne_I(self):
+	self.assertEqual(numberToRoman(1),"I")
 
 if __name__ == '__main__':
         unittest.main()
