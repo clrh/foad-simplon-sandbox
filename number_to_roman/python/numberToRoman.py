eaa357cc0426a9610bf6c79feb8b881122e9b336
Claire Hernandez <claire.hernandez@biblibre.com>
2014-11-19 17:06:34 +0100

    red

---
+++ number_to_roman/python/numberToRoman.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
+
+import unittest
+class TestLags(unittest.TestCase):
+    def test_test(self):
+        self.assertEqual(True, False)
+
+if __name__ == '__main__':
+        unittest.main()
