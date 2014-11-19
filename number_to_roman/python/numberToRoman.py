8863fcb992ad91929efe354664c93fa09b3cfa65
Claire Hernandez <claire.hernandez@biblibre.com>
2014-11-19 17:10:26 +0100

    red

--- number_to_roman/python/numberToRoman.py
+++ number_to_roman/python/numberToRoman.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
+# vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
 
 import unittest
 class TestLags(unittest.TestCase):
@@ -8,6 +8,10 @@ class TestLags(unittest.TestCase):
         self.assertEqual(True, True)
     def test_1_donne_I(self):
 	self.assertEqual(numberToRoman(1),"I")
+
+def numberToRoman(number):
+	if number == 1:
+		return "I"
 
 if __name__ == '__main__':
         unittest.main()
