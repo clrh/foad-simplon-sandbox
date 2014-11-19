5ee3425f7031dfcbc02705693fa7668e152e3916
Claire Hernandez <claire.hernandez@biblibre.com>
2014-11-19 17:25:57 +0100

    base python test fails

---
+++ modeles/base.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
+
+import unittest
+class TestLags(unittest.TestCase):
+    def test_test(self):
+        self.assertEqual(False, True)
+
+if __name__ == '__main__':
+        unittest.main()
