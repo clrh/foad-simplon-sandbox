a7dd9cbc63707e3dcdd9bd936d17f521a272de34
clrh <claire.hernandez+welaan@gmail.com>
2014-12-03 22:16:40 +0100

    wip

--- railsform/app/controllers/forms_controller.rb
+++ railsform/app/controllers/forms_controller.rb
@@ -4,12 +4,13 @@ class FormsController < ApplicationController
   
   def create
     @form = Form.new(form_params)
+
     @form.save
-    redirect_to form_path
+    redirect_to forms_path
   end
    
   private
     def form_params
-        params.require(:form).permit(:title, :comment)
+        params.require(:form).permit(:title)
     end
 end
